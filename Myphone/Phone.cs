24a67814ce91d7bcecece10daa3c34de081f89b9
Denis <denkul87@gmail.com>
2020-09-29 15:42:52 +0300

    !

---
+++ Myphone/Phone.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+namespace Myphone
+{
+    public delegate void deRecv(byte data);
+   abstract class Phone
+    {
+        protected NetworkStream ns;
+        protected int port;
+        protected string host;
+        public deRecv Recv;
+
+        public void Start()
+        {
+            Thread thread = new Thread(Waiter);
+            thread.Start();
+        }
+        public void Waiter()
+        {
+            while (true)
+            {
+                Connect();
+                while (true)
+                {
+                    
+                try
+                    {
+                        int data = ns.ReadByte();
+                        if (data != -1)
+                            Recv((byte)data);
+                    }
+                    catch
+                    {
+                        Thread.Sleep(100);
+                        break;
+                    }
+                }
+            }
+        }
+        abstract public void Connect ();
+     
+        public bool Send(byte data)
+        {
+            try
+            {
+                ns.WriteByte(data);
+                Console.WriteLine("Send " + data.ToString());
+                return true;
+            }
+            catch
+            {
+                Thread.Sleep(100);
+                Console.WriteLine("Error sending " + data.ToString());
+                return false;
+
+            }
+        }
+    }
+    class PhoneServer : Phone
+    {
+        public PhoneServer(int port)
+        {
+            this.port = port;
+        }
+       override public void Connect()
+        {
+            try
+            {
+                Console.WriteLine("Starting Server");
+                TcpListener listener = new TcpListener(IPAddress.Any, port);
+                listener.Start();
+                TcpClient tcpClient = listener.AcceptTcpClient();
+                ns = tcpClient.GetStream();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+    class PhoneClient : Phone
+    {
+        public PhoneClient(string host, int port)
+        {
+            this.host = host;
+            this.port = port;
+        }
+       override public void Connect()
+        {
+            try
+            {
+                Console.WriteLine("Starting Server");
+                TcpClient tcpClient = new TcpClient(host, port);
+                ns = tcpClient.GetStream();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
---
+++ Myphone/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Myphone
+{
+    class Program
+    {
+        Random rand = new Random();
+        static int port = 8000;
+        static string host = "127.0.0.1";
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+            program.Run();
+            Console.WriteLine("Stop");
+            Console.ReadKey();
+        }
+        public void Run()
+        {
+            Console.Write("1 : Server, 2 : Client >");
+            string str = Console.ReadLine();
+            Phone phone;
+
+            if (str == "1")
+                phone = new PhoneServer(port);
+            else phone = new PhoneClient(host, port);
+            phone.Recv += Recv1;
+            phone.Start();
+            while (true)
+            {
+                phone.Send((byte)rand.Next(10, 100));
+                Thread.Sleep(2000);
+            }
+        }
+        public void Recv1(byte data)
+        {
+            Console.WriteLine("Recv" + data.ToString());
+
+        }
+    }
+}
